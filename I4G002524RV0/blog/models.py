68b1db33b1624012adf255810f71308e23bdaeb3
Awal Umar <76787760+drizla01@users.noreply.github.com>
2022-06-21 01:30:03 +0100

    Update models.py

--- I4G002524RV0/blog/models.py
+++ I4G002524RV0/blog/models.py
@@ -1,13 +1,13 @@
 import imp
 from statistics import mode
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import get_user_model
 from matplotlib.pyplot import title
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(auto_now_add=True)
